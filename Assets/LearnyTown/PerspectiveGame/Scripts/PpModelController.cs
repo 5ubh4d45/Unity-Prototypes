3d5f93f8255aafd8a509624414ade2dfbcd7ab39
Subha Das <65977621+5ubh4d45@users.noreply.github.com>
2022-05-04 10:23:43 +0530

    Added the liquid volume shader package

---
+++ Assets/LearnyTown/PerspectiveGame/Scripts/PpModelController.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace LearnyTown.PerspectiveGame
+{
+    public class PpModelController : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
